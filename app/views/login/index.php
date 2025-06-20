473b4dab60372c593eadf662a7aece4f186d17c2
jshergill <jshergill@algomau.ca>
2025-06-20 03:07:23 +0000

    error and success alert on login page

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -10,6 +10,15 @@
 
 <div class="row">
     <div class="col-sm-auto">
+			<?php if (isset($_SESSION['success'])): ?>
+				<div class="alert alert-success"><?= $_SESSION['success'] ?></div>
+				<?php unset($_SESSION['success']); ?>
+			<?php endif; ?>
+
+			<?php if (isset($_SESSION['error'])): ?>
+				<div class="alert alert-danger"><?= $_SESSION['error'] ?></div>
+				<?php unset($_SESSION['error']); ?>
+			<?php endif; ?>
 		<form action="/login/verify" method="post" >
 		<fieldset>
 			<div class="form-group">
